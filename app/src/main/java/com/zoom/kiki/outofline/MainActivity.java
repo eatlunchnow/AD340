d187faed4f716a9155c8e2a2ad344d19009c2f88
eatlunchnow <eatlunchnow@gmail.com>
2019-04-16 13:11:25 -0700

    HW2

--- app/src/main/java/com/zoom/kiki/outofline/MainActivity.java
+++ app/src/main/java/com/zoom/kiki/outofline/MainActivity.java
@@ -3,11 +3,19 @@ package com.zoom.kiki.outofline;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button toastButton1;
+    private Button toastButton2;
+    public static final String EXTRA_TEXT = "com.zoom.kiki.outofline.EXTRA_TEXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +23,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
+        toastButton1 = (Button) findViewById(R.id.toastButton1);
+        toastButton2 = (Button) findViewById(R.id.toastButton2);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSecondActivity();
             }
         });
+
+        toastButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast();
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_root));
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+
+                toast.show();
+
+            }
+        });
+
+        toastButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast();
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast_layout2, (ViewGroup) findViewById(R.id.toast_root2));
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+
+                toast.show();
+
+            }
+        });
+    }
+
+
+    public void showToast(){
+
     }
 
     public void openSecondActivity(){
+        EditText editText1 = (EditText) findViewById(R.id.EditText1);
+        String text = editText1.getText().toString();
+
         Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_TEXT, text);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/zoom/kiki/outofline/SecondActivity.java
+++ app/src/main/java/com/zoom/kiki/outofline/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.zoom.kiki.outofline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+
+        TextView textview1 = (TextView) findViewById(R.id.textView1);
+
+        textview1.setText(text);
     }
 }

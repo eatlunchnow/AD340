828202cb49417787174bae27fd9c5c07d5e975d6
eatlunchnow <eatlunchnow@gmail.com>
2019-06-10 14:00:58 -0700

    HW5 - 6/10/19

--- app/src/main/java/com/zoom/kiki/outofline/About.java
+++ app/src/main/java/com/zoom/kiki/outofline/About.java
@@ -1,6 +1,6 @@
 package com.zoom.kiki.outofline;
 
-<<<<<<< Updated upstream
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,48 +9,23 @@ import android.support.v7.app.ActionBar;
 import android.view.MenuInflater;
 public class About extends AppCompatActivity {
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        //connecting the toolbar layout
-=======
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-
-public class About extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_about);
+            //connecting the toolbar layout
+            Toolbar toolbar = findViewById(R.id.app_bar);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle("About");
 
->>>>>>> Stashed changes
-        Toolbar toolbar = findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("About");
-
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
-
-
-<<<<<<< Updated upstream
-        };
-=======
-    };
->>>>>>> Stashed changes
+            ActionBar ab = getSupportActionBar();
+            ab.setDisplayHomeAsUpEnabled(true);
 
 
+        }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_movie_list_scrolling, menu);
-        return super.onCreateOptionsMenu(menu);
     }
-}
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
+
+
+
